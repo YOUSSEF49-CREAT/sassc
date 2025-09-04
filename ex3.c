f0b6316b0f900afd325eb73a9f06f7b96449676e
youssef faouzi <yj1984515@gmail.com>
2025-09-04 09:59:33 +0200

    ex 3

---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int i , n ;
+    printf(entrer un nomber de n :);
+    scanf(%d, &n);
+    for(i=1;i<n*2;i++){
+         printf(%dn,i);
+       i++;
+    }
+     
+    return 0;
+} 
